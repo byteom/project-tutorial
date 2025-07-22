b6f70d66f13542d1614e8f9c2c6a153a6aaa79c2
Om Singh <123omsinghpipariya@gmail.com>
2025-07-22 04:35:08 +0000

    Module not found: Can't resolve 'rehype-prism-plus/themes/dracula.css'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
-import "rehype-prism-plus/themes/dracula.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/layout/Header";
